b258f1089fdf965bb5179c98e837cbd341b793e2
WeirdOddz <OscarPeachMan@outlook.com>
2024-08-10 22:09:48 +1000

    Day 1: Basics completed

--- server.py
+++ server.py
@@ -2,24 +2,28 @@ import flask
 import flask_cors
 import json
 
+def send_file(directory):
+    with open(directory, "r") as file:
+        return file.read()
+
 app = flask.Flask(__name__)
-CORS = flask_cors.CORS(app)
+#CORS = flask_cors.CORS(app)
 
 @app.route("/")
 def index():
-    return flask.send_file("site/index.html")
+    return send_file("site/index.html")
 
 @app.route("/app.js")
 def js():
-    return flask.send_file("site/app.js")
+    return send_file("site/app.js")
 
 @app.route("/styles.css")
 def styles():
-    return flask.send_file("site/styles.css")
+    return flask.send_file("site/styles.css", mimetype="text/css")
 
 @app.route("/words_dictionary.json")
 def dictWords():
-    return flask.send_file("site/words_dictionary.json")
+    return send_file("site/words_dictionary.json")
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=4996)
--- site/app.js
+++ site/app.js
@@ -1,20 +1,50 @@
 console.log("APP")
 
 document.addEventListener("DOMContentLoaded", async function main() {
-    const menu = document.getElementById("menu")
+    const menu = document.getElementById("mainMenu")
     const downloading = document.getElementById("notice")
     const game = document.getElementById("game")
     const start = document.getElementById("start")
+    const input = document.getElementById("iput")
+    const submit = document.getElementById("submit")
+    const registered = document.getElementById("wordsCollected")
 
     const wordDictionaryF = await fetch("words_dictionary.json")
-    const wordDictionary = await wordDictionary.json()
+    const wordDictionary = await wordDictionaryF.json()
     downloading.remove()
 
     start.addEventListener("click", async function(){
         console.log("Clicked")
-        const keys = Object.keys(wordDictionary);
-        const randomIndex = Math.floor(Math.random() * keys.length);
-        console.log(randomIndex)
+        function word() {
+            const keys = Object.keys(wordDictionary);
+            const randomIndex = keys[Math.floor(Math.random() * keys.length)];
+            const randomValue = wordDictionary[randomIndex]
+
+            return randomIndex
+        }
+
+        function checkWord(word) {
+            let correct = false;
+            if (wordDictionary[word] == 1) {
+                correct = true
+            }
+            return correct
+        }
+        menu.style.visibility = "hidden"
+        game.style.visibility = "visible"
+
+        registry = []
+
+        submit.addEventListener("click", function(){
+            const inp = input.value
+            
+            if (checkWord(inp) && !(inp in registry)) {
+                let register = document.createElement("p")
+                registry.push(inp)
+                register.innerText = inp
+                registered.appendChild(register)
+            }
+        })
     })
 
 })
